068a1cec14b061c2e26f84f9c8bec5cca364c0ec
Jana Simashkevich <jsimashk@gmail.com>
2020-02-08 13:31:28 -0500

    updates

---
+++ model.py
@@ -0,0 +1,26 @@
+from .app import db
+
+class ParkingSpot(db.Model):
+    __tablename__ = 'share_parking_spots'
+
+    garage_id = db.Column(db.String(64), primary_key=True)
+    address = db.Column(db.String(64))
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+    rate_per_half_hour = db.Column(db.Float)
+    carpark_type_str = db.Column(db.String(64))
+    capacity = db.Column(db.Integer)
+    start_date_time = db.Column(db.String(64))
+    start_date = db.Column(db.String(64))
+    start_time = db.Column(db.String(64))
+    end_date_time = db.Column(db.String(64))
+    end_date = db.Column(db.String(64))
+    end_time = db.Column(db.String(64))
+    duration_type = db.Column(db.String(64))
+    parking_type = db.Column(db.String(64))
+    cust_id = db.Column(db.String(64))
+    cell = db.Column(db.String(64))
+
+
+    def __repr__(self):
+        return '<ParkingSpot %r>' % (self.garage_id)
