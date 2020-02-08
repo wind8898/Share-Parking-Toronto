41591af99c35f39fe46f2b2a64edce47c92e55f4
wind8898 <wind8898@hotmail.com>
2020-02-08 10:36:08 -0500

    update

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoid2luZDg4OTgiLCJhIjoiY2s1eDQ1M2NuMHN2YjNtcnNhd2Z4b28yOCJ9.PfuOXFf4xjjA3pH4nrf2Tw"
---
+++ static/js/logic.js
@@ -0,0 +1,12 @@
+var myMap = L.map("map", {
+    center: [43.651070, -79.347015],
+    zoom: 8
+  });
+  
+  // Add a tile layer
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap);
