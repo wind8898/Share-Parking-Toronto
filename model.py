4ee6ab8789e475bf1f0d440284f41b89a800d4cd
Jana Simashkevich <jsimashk@gmail.com>
2020-02-11 15:54:33 -0500

    map layers

--- model.py
+++ model.py
@@ -3,24 +3,24 @@ from app import db
 class ParkingSpot(db.Model):
     __tablename__ = 'parking_share_spots'
 
-    garage_id = db.Column(db.String(64), primary_key=True)
+    garage_id = db.Column(db.Integer, primary_key = True)
     address = db.Column(db.String(64))
     lat = db.Column(db.Float)
     lng = db.Column(db.Float)
     rate_per_half_hour = db.Column(db.Float)
     carpark_type_str = db.Column(db.String(64))
     capacity = db.Column(db.Integer)
-    start_date_time = db.Column(db.String(64), primary_key=True)
+    start_date_time = db.Column(db.String(64))
     start_date = db.Column(db.String(64))
     start_time = db.Column(db.String(64))
-    end_date_time = db.Column(db.String(64), primary_key=True)
+    end_date_time = db.Column(db.String(64))
     end_date = db.Column(db.String(64))
     end_time = db.Column(db.String(64))
     duration_type = db.Column(db.String(64))
     parking_type = db.Column(db.String(64))
-    cust_id = db.Column(db.String(64), primary_key=True)
+    cust_id = db.Column(db.Integer)
     cell = db.Column(db.String(64))
 
 
     def __repr__(self):
-        return '<ParkingSpot %r>' % (self.garage_id)
+        return '<ParkingSpot %r>' % (self.address)
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,24 +1,47 @@
-var myMap = L.map("map", {
+/*var myMap = L.map("map", {
     center: [43.651070, -79.347015],
     zoom: 12
   });
-  
+  */
   // Add a tile layer
  /* L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 10,
     id: "mapbox.streets",
     accessToken: API_KEY
-  }).addTo(myMap);*/
+  }).addTo(myMap);
 
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 100,
     id: "mapbox.streets",
     accessToken: API_KEY
-  }).addTo(myMap);
+  }).addTo(myMap);*/
 
-  function addMarkers() {
+  
+// Define variables for our base layers
+var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
+var comicmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.comic",
+  accessToken: API_KEY
+});
+var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.satellite",
+  accessToken: API_KEY
+});
+
+parkingMarkers = [];
+
+  //function addMarkers() {
     /* data route */
   var url = "/api/get_parking_spots";
   d3.json(url).then(function(response) {
@@ -32,16 +55,19 @@ var myMap = L.map("map", {
     var hoverText = [];
 
   // Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
-  for (var i = 0; i < response[0].address.length; i++) {
+  /*for (var i = 0; i < response[0].address.length; i++) {
     //if(response[0].lat[i]  response[0].lng[i]])
     //coordinates.push([response[0].lat[i], response[0].lng[i]]);
     coordinates = [response[0].lat[i], response[0].lng[i]];
-    L.marker(coordinates)
+    parkingMarkers.push(L.marker(coordinates)
       .bindPopup("<h3>Address:" + response[0].address[i] + "</h3> <hr> <h3>Price per 30 min: " + esponse[0].price_per_half_hour[i]+ "</h3>")
-      .addTo(myMap);
+    )
+      //.addTo(myMap);
     //hoverText.push("<h1>Garage ID:" + response[0].garage_id[i] + "</h1> <hr> <h3>Price per 30 min: " + response[0].price_per_half_hour[i] + "</h3>");
-  }
 
+
+  }*/
+    
    // console.log(coordinates);
     //console.log(hoverText);
 
@@ -50,6 +76,38 @@ var myMap = L.map("map", {
       //.addTo(myMap);
     
   });
-}
 
-addMarkers();
+  //return parkingMarkers;
+//}
+
+
+//addMarkers();
+
+var cityLayer = L.layerGroup(parkingMarkers);
+
+  
+// Create a baseMaps object
+var baseMaps = {
+  "Street Map": streetmap,
+  "Comic  Map": comicmap,
+  "Satellite Map": satellitemap
+};
+
+// Overlays that may be toggled on or off
+var overlayMaps = {
+  Parking: cityLayer
+};
+
+// Define a map object
+var myMap = L.map("map", {
+  center: [43.6683,-79.3977],
+  zoom: 14,
+  layers: [streetmap, cityLayer]
+});
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed:false
+}).addTo(myMap);
+
+
