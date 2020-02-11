1c960db40e32cbcb76c2d2e769796256f72bca13
Jana Simashkevich <jsimashk@gmail.com>
2020-02-10 20:44:43 -0500

    updates

--- app.py
+++ app.py
@@ -19,6 +19,37 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation w
 
 db = SQLAlchemy(app)
 
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+
+        garage_id = getNextGarageID() #function call to get next id
+        address = request.form["address"]
+        lat = request.form["lat"] #google api from location
+        lng = request.form["lng"] #google api from location
+        rate_per_half_hour = request.form["rate_per_half_hour"] 
+        carpark_type_str = request.form["carpark_type_str"]
+        capacity = request.form["capacity"]
+        start_date_time = request.form["start_date_time"]
+        start_date = request.form["start_date"]
+        start_time = request.form["start_time"]
+        end_date_time = request.form["end_date_time"]
+        end_date = request.form["end_date"]
+        end_time = request.form["end_time"]
+        duration_type = request.form["duration_type"]
+        parking_type = request.form["parking_type"]
+        cust_id = getCustomerID(request.form["cell"])
+        cell = request.form["cell"]
+        
+        parking_spot = ParkingSpot(garage_id=garage_id, address=address, lat=lat, lng=lng, rate_per_half_hour=rate_per_half_hour,
+                                    carpark_type_str=carpark_type_str, capacity=capacity, start_date_time=start_date_time, start_date=start_date, 
+                                    start_time=start_time, end_date_time=end_date_time, end_date=end_date, end_time=end_time, 
+                                    duration_type=duration_type, parking_type=parking_type,cust_id=cust_id, cell=cell)
+        db.session.add(parking_spot)
+        db.session.commit()
+        return redirect("/", code=302)
+
+    return render_template("form.html")
 
 
 @app.route("/api/get_parking_spots")
@@ -45,3 +76,13 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug = True)
+
+
+def getNextGarageID():
+    max_id = db.session.query(db.func.max(ParkingSpot.garage_id)).all()
+    print(max_id)
+
+def getCustomerID(cell):
+    cust_id = db.session.query(ParkingSpot.cust_id).filter(ParkingSpot.cell == cell).all()
+    #if blank generate new id
+    
